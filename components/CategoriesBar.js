85c6f5154e072695ffa5edab267cde35e084859d
Sudheer Papineni <55796541+sudir81@users.noreply.github.com>
2021-09-30 09:06:04 -0400

    HomeScreen -> CategoriesBar & Assets

--- components/CategoriesBar.js
+++ components/CategoriesBar.js
@@ -1,10 +1,63 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+
+const items = [
+  {
+    image: require("../assets/images/shopping-bag.png"),
+    text: "Pick-up",
+  },
+  {
+    image: require("../assets/images/soft-drink.png"),
+    text: "Soft Drinks",
+  },
+  {
+    image: require("../assets/images/bread.png"),
+    text: "Bakery Items",
+  },
+  {
+    image: require("../assets/images/fast-food.png"),
+    text: "Fast Foods",
+  },
+  {
+    image: require("../assets/images/deals.png"),
+    text: "Deals",
+  },
+  {
+    image: require("../assets/images/coffee.png"),
+    text: "Coffee & Tea",
+  },
+  {
+    image: require("../assets/images/desserts.png"),
+    text: "Desserts",
+  },
+];
 
 export default function CategoriesBar() {
+  const itemSelected = (item) => {
+    console.log(item.text);
+  };
+
   return (
     <View>
-      <Text></Text>
+      <FlatList
+        horizontal
+        data={items}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => itemSelected(item)}>
+            <View style={{ alignItems: "center", marginRight: 30 }}>
+              <Image
+                source={item.image}
+                style={{ width: 50, height: 40, resizeMode: "contain" }}
+              />
+              <Text style={{ fontSize: 13, fontWeight: "900" }}>
+                {item.text}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        )}
+        style={{ marginTop: 10 }}
+      />
     </View>
   );
 }
